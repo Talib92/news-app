3538c1b26552f037d21d2e3707fb35129d694483
Talib Mukhtar <talibmukhtar54@gmail.com>
2024-09-15 12:33:43 +0530

    created store for theme

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import articlesSlice from "./articlesSlice";
+import themeSlice from "./themeSlice";
 
 const store = configureStore({
     reducer:{
         article:articlesSlice,
+        theme:themeSlice
     }
 })
 
