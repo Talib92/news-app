8c1ef52fa2d352a2bbabf87fe3262a08c7e3197f
Talib Mukhtar <talibmukhtar54@gmail.com>
2024-09-15 14:36:23 +0530

    minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import NewsCard from './NewsCard';
 import Shimmer from "./Shimmer"
 import LoadMore from './LoadMore';
@@ -10,7 +10,6 @@ const Home = () => {
 
   const articles = useSelector((state) => state.article.articles);
   const theme = useSelector((state)=> state.theme.theme);
-
   const [count, setCount] = useState(21);
 
   useHomeNews();
@@ -23,23 +22,21 @@ const Home = () => {
 
   return articles.length === 0 ? <Shimmer/> : (
     <>
-    <div className={theme === "Light" ? "bg-white text-black" : "bg-neutral-950 text-white"}>
+      <div className={theme === "Light" ? "bg-white text-black" : "bg-neutral-950 text-white"}>
 
-    <div className='md:pt-4 pt-16'>
-        <h1 className={`text-3xl font-semibold ml-2 mb-4 text-center mt-16 ${theme === "Light" ? "text-black" : "text-white"}`}>News around the world 🌎</h1>
+        <div className='md:pt-4 pt-16'>
+          <h1 className={`text-3xl font-semibold ml-2 mb-4 text-center mt-16 ${theme === "Light" ? "text-black" : "text-white"}`}>News around the world 🌎</h1>
 
-        <div className='flex flex-wrap lg:gap-12 lg:ml-0 md:gap-2 w-full md:-ml-6 ml-4 gap-4'>
+          <div className='flex flex-wrap lg:gap-12 lg:ml-0 md:gap-2 w-full md:-ml-6 ml-4 gap-4'>
 
-        {articles.slice(0,count).map((news,index) =>(
-          <NewsCard key={index} news = {news} theme={theme}/>
-        ))}
-
-        </div>
-
-        <LoadMore loadMore={loadMore} count={count} totalLength={articles.length} theme={theme} />
-
-    </div>
-    </div>
+            {articles.slice(0,count).map((news,index) =>(
+              <NewsCard key={index} news = {news} theme={theme}/>
+            ))};
+          </div>
+            
+            <LoadMore loadMore={loadMore} count={count} totalLength={articles.length} theme={theme} />
+          </div>
+      </div>
     </>
   )
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import NewsCard from './NewsCard';
 import Shimmer from './Shimmer';
 import LoadMore from './LoadMore';
@@ -12,7 +12,6 @@ const Results = () => {
   const {name} = useParams(); 
   const articles = useSelector((state) => state.article.articles);
   const theme = useSelector((state)=> state.theme.theme);
-  
   const [count, setCount] = useState(21);
 
   useResultsNews();
@@ -26,19 +25,17 @@ const Results = () => {
   return articles.length === 0 ? <Shimmer/> : (
 
     <div>
-
       <div className={`${theme === "Light" ? "bg-white text-black" : "bg-neutral-900 text-white"} md:pt-20 pt-32`}>
-      <h1 className='text-3xl font-semibold ml-2 mb-4 text-center'>News about {name}.</h1>
+        <h1 className='text-3xl font-semibold ml-2 mb-4 text-center'>News about {name}.</h1>
 
-      <div className='flex flex-wrap lg:gap-12 lg:ml-0 md:gap-2 w-full md:-ml-6 ml-4 gap-4'>
+        <div className='flex flex-wrap lg:gap-12 lg:ml-0 md:gap-2 w-full md:-ml-6 ml-4 gap-4'>
 
-        {articles.slice(0,count).map((news,index) =>(
-          <NewsCard key={index} news = {news} theme={theme}/>
-        ))}
+          {articles.slice(0,count).map((news,index) =>(
+            <NewsCard key={index} news = {news} theme={theme}/>
+          ))};
+        </div>
+          <LoadMore loadMore={loadMore} count={count} totalLength={articles.length} theme={theme} />
       </div>
-      
-      <LoadMore loadMore={loadMore} count={count} totalLength={articles.length} theme={theme} />
-    </div>
     </div>
   )
 }
