4c831669e23d5eac8dbfcdaddac4e6750450ea12
Talib Mukhtar <talibmukhtar54@gmail.com>
2024-09-16 07:10:03 +0530

    minor changes

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -10,22 +10,23 @@ const NewsCard = ({news, theme}) => {
   return (
 
     <a href={url} target='_blank' rel="noopener noreferrer"> 
-    
-    <div className={`flex ml-16 rounded-xl shadow-lg h-auto 
+
+    <div className={`flex ml-16 rounded-xl shadow-lg h-full
       ${theme === "Light" ? "bg-gray-50 text-black hover:bg-gray-100" : "bg-neutral-800 text-white hover:bg-neutral-900"}`}>
       <div className={`px-2 py-2 shadow-lg hover:shadow-2xl`}>
         
         <div>
-          <img className='w-[18rem] rounded-md h-48' src={ imageUrl} alt="image" />
+          <img className='w-[18rem] rounded-md h-48' src={ imageUrl} alt="thumbnail" />
         </div>
 
         <div className='mt-2'>
-          <p className="w-[18rem]">{title}</p>
+          <p className="w-[18rem] font-semibold">{title}</p>
           <p className="w-[18rem] mt-2">{description}</p>
         </div>
 
       </div>
     </div>
+
     </a>
   )
 }
